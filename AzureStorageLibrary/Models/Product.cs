aa50f715bb1f64f47a20b48772c5e08f6d764d6a
Tural Novruzov <TuraliNovruzov@outlook.com>
2020-09-27 03:37:20 +0400

    Start

---
+++ AzureStorageLibrary/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Product : TableEntity
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
