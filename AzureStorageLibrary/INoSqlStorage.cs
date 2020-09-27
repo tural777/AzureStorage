fde9faf9dff2d7f428d70d70d0cb8b8c332858bb
Tural Novruzov <TuraliNovruzov@outlook.com>
2020-09-27 04:04:08 +0400

    Add INoSqlStorage Interface

---
+++ AzureStorageLibrary/INoSqlStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary
+{
+    interface INoSqlStorage<TEntity>
+    {
+        Task<TEntity> Add(TEntity entity);
+
+        Task Delete(string rowKey, string partitionKey);
+
+        Task<TEntity> Update(TEntity entity);
+
+        Task<TEntity> Get(string rowKey, string partitionKey);
+
+        Task<IQueryable<TEntity>> All();
+
+        IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> query);
+
+    }
+}
